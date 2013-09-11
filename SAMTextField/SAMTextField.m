4f7882fce19953598b45134a8f8b430c5f904950
Sam Soffes <sam@soff.es>
2013-09-10 17:40:34 -0700

    Remove placeholder color in favor of attributedPlaceholder in iOS 7

--- SAMTextField/SAMTextField.m
+++ SAMTextField/SAMTextField.m
@@ -10,17 +10,6 @@
 
 @implementation SAMTextField
 
-#pragma mark - Accessors
-
-- (void)setPlaceholderTextColor:(UIColor *)placeholderTextColor {
-	_placeholderTextColor = placeholderTextColor;
-
-	if (!self.text && self.placeholder) {
-		[self setNeedsDisplay];
-	}
-}
-
-
 #pragma mark - UIView
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
@@ -42,39 +31,28 @@
 #pragma mark - UITextField
 
 - (CGRect)textRectForBounds:(CGRect)bounds {
-	return UIEdgeInsetsInsetRect([super textRectForBounds:bounds], self.textEdgeInsets);
+  return UIEdgeInsetsInsetRect([super textRectForBounds:bounds], self.textEdgeInsets);
 }
 
 
 - (CGRect)editingRectForBounds:(CGRect)bounds {
-	return [self textRectForBounds:bounds];
+  return [self textRectForBounds:bounds];
 }
 
 
 - (CGRect)clearButtonRectForBounds:(CGRect)bounds {
-	CGRect rect = [super clearButtonRectForBounds:bounds];
-	rect.origin.x += self.clearButtonEdgeInsets.right;
-	rect.origin.y += self.clearButtonEdgeInsets.top;
-	return rect;
-}
-
-
-- (void)drawPlaceholderInRect:(CGRect)rect {
-	if (!self.placeholderTextColor) {
-		[super drawPlaceholderInRect:rect];
-		return;
-	}
-
-    [self.placeholderTextColor setFill];
-    [self.placeholder drawInRect:rect withFont:self.font lineBreakMode:NSLineBreakByTruncatingTail alignment:self.textAlignment];
+  CGRect rect = [super clearButtonRectForBounds:bounds];
+  rect.origin.x += self.clearButtonEdgeInsets.right;
+  rect.origin.y += self.clearButtonEdgeInsets.top;
+  return rect;
 }
 
 
 #pragma mark - Private
 
 - (void)initialize {
-	self.textEdgeInsets = UIEdgeInsetsZero;
-	self.clearButtonEdgeInsets = UIEdgeInsetsZero;
+  self.textEdgeInsets = UIEdgeInsetsZero;
+  self.clearButtonEdgeInsets = UIEdgeInsetsZero;
 }
 
 @end
